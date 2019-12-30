886dfc3d5b98b07ab04987cc177942a2102f7ecb
yinpatt <yinpatt@gmail.com>
2019-12-30 15:43:50 +0800

    Update run.py

--- code/run.py
+++ code/run.py
@@ -1,9 +1,10 @@
 import scrap
 import pandas as pd
+all_stock_code = list(pd.read_csv('https://storage.googleapis.com/hedgefund/Mainboard%20List.csv').TICKER)
 c = scrap.ccass()
 
 all_tables = []
-for i in l:
+for i in all_stock_code:
   try:
     all_tables.append(c.get_table(i))
   except:
